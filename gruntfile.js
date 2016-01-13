3711ef695adb22e991a7eaad0d182aaf6dd4c308
mitchthorson <mthorson@usatoday.com>
2016-01-13 16:59:49 -0500

    update gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -315,7 +315,7 @@ module.exports = function(grunt) {
       },
       upload3: {
         files: {
-          '/17200/experiments/usatoday/2016/01/election-coloring-book/': 'data/*.json'
+          // '/17200/experiments/usatoday/2016/01/election-coloring-book/': 'data/*.json'
         }
       }
     },
