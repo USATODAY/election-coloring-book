2d941089ddb8ca4bea515c35e9680ef72dea1211
mitchthorson <mthorson@usatoday.com>
2016-01-27 11:35:23 -0500

    add share buttons and head and chatter

--- src/js/app.js
+++ src/js/app.js
@@ -3,24 +3,79 @@ define(
     'jquery',
     'underscore',
     'templates',
+    'api/analytics',
+    'config',
     'jquery-turn'
   ],
-  function(jQuery, _, templates){
+  function(jQuery, _, templates, Analytics, config){
     var app = app || {};
 
+    var $sharePopups;
+
+    var SHARE_TEXT = 'Check out USA TODAYs 2016 political coloring book';
+    var DOWNLOAD_URL = 'http://www.gannett-cdn.com/experiments/usatoday/2016/01/election-coloring-book/color_book_print.pdf.zip';
+
+    var maxWidth = 1000;
     app.init = function() {
-      setupBook();
+        setupBook();
+        jQuery('.iapp-page-share-wrap').html(templates['share.html'](createShare(SHARE_TEXT)));
+        $sharePopups = jQuery('.iapp-share-popup');
+        addEvents();
     };
+
+    function addEvents() {
+        $sharePopups.on('click', function(e) {
+            console.log('share');
+            e.preventDefault();
+            Analytics.trackEvent('2016-coloring-book-share-clicked');
+            windowPopup(e.currentTarget.href, 500, 300);
+        });
+        window.addEventListener('resize', _.throttle(setupBook, 500));
+    }
+
 
     function setupBook() {
         var winWidth = window.innerWidth;
-        var width = winWidth < 800 ? winWidth : 800;
+        var width = winWidth < maxWidth ? winWidth : maxWidth;
         jQuery("#flipbook").turn({
             width: width,
             height: width / 1.6,
             autoCenter: true
         });
     }
+
+    function createShare(shareString) {
+        var shareURL = window.location.href;
+        var fbShareURL = encodeURI(shareURL.replace('#', '%23'));
+        var twitterShareURL = encodeURIComponent(shareURL);
+        var emailLink = "mailto:?body=" + encodeURIComponent(shareString) +  "%0d%0d" + twitterShareURL + "&subject=";
+        
+        return {
+            'fb_id': config.fb_app_id,
+            fbShare:  encodeURI(shareURL.replace('#', '%23')),
+            stillimage: "http://www.gannett-cdn.com/experiments/usatoday/2015/05/broadway/images/fb-post.jpg",
+            encodedShare: encodeURIComponent(shareString),
+            fb_redirect: 'http://' + window.location.hostname + '/pages/interactives/fb-share/',
+            email_link: "mailto:?body=" + encodeURIComponent(shareString) +  "%0d%0d" + encodeURIComponent(shareURL) + "&subject=",
+            download_url: DOWNLOAD_URL,
+            twitterShare: encodeURIComponent(shareURL)
+        };
+
+    }
+
+    function windowPopup(url, width, height) {
+        // Calculate the position of the popup so
+        // it’s centered on the screen.
+        var left = (screen.width / 2) - (width / 2),
+        top = (screen.height / 2) - (height / 2);
+
+        window.open(
+            url,
+            "",
+            "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=" + width + ",height=" + height + ",top=" + top + ",left=" + left
+        );
+    }
+
 
 
     return app;
--- src/js/templates.js
+++ src/js/templates.js
@@ -2,6 +2,34 @@ define(function(){
 
 this["templates"] = this["templates"] || {};
 
+this["templates"]["share.html"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<div class="social">\n    <a href="https://twitter.com/intent/tweet?url=' +
+((__t = (twitterShare)) == null ? '' : __t) +
+'&text=' +
+((__t = (encodedShare)) == null ? '' : __t) +
+'" class="iapp-share-button iapp-share-twitter iapp-share-popup" target="_blank"><img src="http://www.gannett-cdn.com/experiments/usatoday/2015/06/emoji-index/img/twitter.svg" alt="Twitter share"></a>\n    <a class="iapp-share-button iapp-share-popup" href="https://www.facebook.com/dialog/feed?display=popup&app_id=' +
+((__t = (fb_id)) == null ? '' : __t) +
+'&link=' +
+((__t = (fbShare)) == null ? '' : __t) +
+'&picture=' +
+((__t = (stillimage)) == null ? '' : __t) +
+'&name=&description=' +
+((__t = (encodedShare)) == null ? '' : __t) +
+'&redirect_uri=' +
+((__t = (fb_redirect)) == null ? '' : __t) +
+'"><img src="http://www.gannett-cdn.com/experiments/usatoday/2015/05/broadway/images/fb.svg" class="" alt="Share"  border="0"></a>\n    <a href="' +
+((__t = ( download_url )) == null ? '' : __t) +
+'" download="' +
+((__t = ( download_url )) == null ? '' : __t) +
+'" class="iapp-share-button iapp-download"><img src="http://www.gannett-cdn.com/experiments/usatoday/2015/06/emoji-index/img/download-icon.svg" alt="Download"></a>\n</div>\n';
+
+}
+return __p
+};
+
 this["templates"]["template.html"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
